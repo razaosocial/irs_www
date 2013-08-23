f38f701ab205691a3ae237cac2d2120ff6437aa8
brunasoder <bruna.soder@razaosocial.org.br>
2013-08-23 17:11:17 -0300

    conteudo pagina progetos

--- projetos.php
+++ projetos.php
@@ -3,14 +3,184 @@
    include_once('inc/menu.php');
  ?>
 
--  <div class="container">
--    <h2>RAZAO SOCIAL O CAMINHO PARA O FUTURO!</h2><br />frase superior
--  </div>
+  <div class="container">
+   <h2>RAZAO SOCIAL O CAMINHO PARA O FUTURO!</h2><br />frase superior
+  </div>
  
 <section id="content" class="container">
--    <h5>conteudo do site!!</h5>
+  <div class="filter-portfolio">
+				<ul id="filterable">
+					<li><a data-categories="*" title="Show All Projects">All</a></li>
+					<li><a data-categories="animation" title="Show Animation Projects">Animation</a></li>
+					<li><a data-categories="design" title="Show All Design Projects">Design</a></li>
+					<li><a data-categories="illustration" title="Show All Illustration Projects">Illustration</a></li>
+					<li><a data-categories="photography" title="Show Photography Projects">Photography</a></li>
+					<li><a data-categories="development" title="Show Development Projects">Development</a></li>
+				</ul>
+  </div>
+			<div class="portfolio-grid">
+				<ul class="portfolio-items">
+					<li data-categories="design development" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Wordpress themes</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="illustration design" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-2.jpg" data-rel="prettyPhoto" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-2.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Logo Design</h3>
+					</a>
+					<p class="meta">
+						Design / Illustration
+					</p>
+					</li>
+					<li data-categories="design development" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-3.jpg" class="screen-roll" data-rel="prettyPhoto" title="Some Image Dummy Title"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-3.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Joomla Plugins</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="photography illustration animation" class="thumb one-fourth">
+					<a href="http://www.youtube.com/watch?v=GgR6dyzkKHI" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>SEO Optimization</h3>
+					</a>
+					<p class="meta">
+						Design / Photography
+					</p>
+					</li>
+					<li data-categories="development design" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-5.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-5.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>E-Commerce</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="design animation" class="thumb one-fourth">
+					<a href="http://vimeo.com/7449107" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-6.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Hosting Solutions</h3>
+					</a>
+					<p class="meta">
+						Design / Animation
+					</p>
+					</li>
+					<li data-categories="development illustration" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-7.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-7.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Site Maintenance</h3>
+					</a>
+					<p class="meta">
+						Design / Illustration
+					</p>
+					</li>
+					<li data-categories="photography animation" class="thumb one-fourth">
+					<a href="http://vimeo.com/7449107" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-8.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Wordpress Plugins</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="illustrator design" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-9.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-9.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Drupal Websites</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="photography animation" class="thumb one-fourth">
+					<a href="http://www.youtube.com/watch?v=GgR6dyzkKHI" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-10.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Magento Shops</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="design illustration development" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-11.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-11.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Jquery Plugins</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+					<li data-categories="photography development illustration" class="thumb one-fourth">
+					<a href="images/portfolio-images/portfolio-item-12.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/portfolio-images/portfolio-item-12.jpg" alt=" "/>
+					</div>
+					</a>
+					<a href="single-project.html">
+					<h3>Wordpress Shops</h3>
+					</a>
+					<p class="meta">
+						Design / Development
+					</p>
+					</li>
+				</ul>
+			</div>
+			<!--CONTENT ENDS-->
 -</section> 
-
+			
 <?php 
 	 include_once('inc/footer.php');
 ?>
