2ee7fc9535de4e9ce135c2c7ec215be7840d6058
Fernando Nogueira <nando_tecnologia@hotmail.com>
2013-08-23 15:03:17 -0300

    Comentando Mensagem acima do slider

--- index.php
+++ index.php
@@ -1,18 +1,314 @@
 <?php
 	include_once('inc/header.php');
 	include_once('inc/menu.php');
-
-
 ?>
-<div class="title-div">
-	<div class="container">
-	  <h2>RAZAO SOCIAL O CAMINHO PARA O FUTURO!</h2><br />frase superior
-	</div>
+<!-- Mensagem -->
+<!--<div class="title-div">
+        <div class="container">
+                <h2>We provide the ultimate <strong>WordPress themes</strong>, that work amazing both on your computers &amp; mobile devices, and we make awesome <strong>HTML templates</strong></h2>
+        </div>
+</div>-->
+</section>
+<section class="container">
+<!-- Start Cute Slider -->
+<section class="cute-section">
+<div id="cute-slider" class="container">
+        <div class="one">
+                <div class="one">
+                        <div id="my-cute-slider" class="cute-slider" data-width="960" data-height="450">
+                                <ul data-type="slides">
+                                        <li data-delay="4" data-trans3d="tr2,tr20,tr57" data-trans2d="tr1,tr14,tr26">
+                                        <img src="images/slider/slider1.jpg" data-thumb="images/slider/thumbs/thumb2.png" alt=""/>
+                                        <div data-type="info" class="cute-caption" data-align="left">
+                                                <div>
+                                                        <h4>Some awesome slide title goes here</h4>
+                                                        <p>
+                                                                Lorem ipsum dolor sit amet, rhenum ibat est se est amet constanter approximavit te. Suos exteriores non dum est se ad suis alteri formam cum magna aliter quem est in.
+                                                        </p>
+                                                </div>
+                                        </div>
+                                        </li>
+                                        <li data-delay="4" data-trans3d="tr17,tr46,tr64" data-trans2d="tr7,tr32,tr40">
+                                        <img src="images/slider/slider2.jpg" data-src="images/slider/slider2.jpg" data-thumb="images/slider/thumbs/thumb2.png" alt=""/>
+                                        <div data-type="info" class="cute-caption" data-align="left">
+                                                <div>
+                                                        <h4>Some awesome slide title goes here</h4>
+                                                        <p>
+                                                                Lorem ipsum dolor sit amet, rhenum ibat est se est amet constanter approximavit te. Suos exteriores non dum est se ad suis alteri formam cum magna aliter quem est in.
+                                                        </p>
+                                                </div>
+                                        </div>
+                                        </li>
+                                        <li data-delay="5" data-trans3d="tr4,tr21,tr62" data-trans2d="tr5,tr23,tr30">
+                                        <img src="images/slider/slider3.jpg" data-src="images/slider/slider3.jpg" data-thumb="images/slider/thumbs/thumb3.png" alt=""/>
+                                        <div data-type="info" class="cute-caption" data-align="left">
+                                                <div>
+                                                        <h4>Some awesome slide title goes here</h4>
+                                                        <p>
+                                                                Lorem ipsum dolor sit amet, rhenum ibat est se est amet constanter approximavit te. Suos exteriores non dum est se ad suis alteri formam cum magna aliter quem est in.
+                                                        </p>
+                                                </div>
+                                        </div>
+                                        </li>
+                                </ul>
+                                <ul data-type="controls">
+                                        <li data-type="captions"></li>
+                                        <li data-type="slideinfo" data-effect="fade"></li>
+                                        <li data-type="circletimer" data-color="#fcaf17" data-stroke="2"></li>
+                                        <li data-type="next"><i class="icon-chevron-right"></i></li>
+                                        <li data-type="previous"><i class="icon-chevron-left"></i></li>
+                                        <li data-type="slidecontrol" data-thumbalign="up"></li>
+                                </ul>
+                        </div>
+                </div>
+        </div>
 </div>
-
- <section id="content" class="container">
-		<h5>conteudo do site!!</h5>
-</section> 
+</section>
+<!-- End Cute Slider -->
+</section>
+<section id="content" class="container">
+<section class="one">
+<div class="horizontal-line">
+</div>
+<div class="title-div">
+        <h2>The Features of <strong>Outshined template</strong></h2>
+        <h5>See What We’ve Been Working On. We work with lots of great companies. Learn about what we do.Lorem ipsum dolor sit amet, longa ferri Lorem post multi ingenio.</h5>
+</div>
+</section>
+<section class="one">
+<div class="one-fourth">
+        <div class="service">
+                <div class="featured_img_01">
+                </div>
+                <h4>Responsive</h4>
+        </div>
+</div>
+<div class="one-fourth">
+        <div class="service">
+                <div class="featured_img_02">
+                </div>
+                <h4>Useful Features</h4>
+        </div>
+</div>
+<div class="one-fourth">
+        <div class="service">
+                <div class="featured_img_03">
+                </div>
+                <h4>Lovely Design</h4>
+        </div>
+</div>
+<div class="one-fourth last">
+        <div class="service">
+                <div class="featured_img_04">
+                </div>
+                <h4>Time Saving</h4>
+        </div>
+</div>
+</section>
+<section class="one">
+<div class="horizontal-line">
+</div>
+<div class="title-div">
+        <h2>Our amazing work <strong>we've created</strong></h2>
+        <h5>See What We’ve Been Working On. We work with lots of great companies. Learn about what we do.Lorem ipsum dolor sit amet, longa ferri Lorem post multi ingenio.</h5>
+</div>
+<div class="portfolio-grid">
+        <ul class="portfolio-items">
+                <li data-categories="design development" class="thumb one-fourth">
+                <a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Wordpress themes</h3>
+                </a>
+                <p class="meta">
+                        Design / Development
+                </p>
+                </li>
+                <li data-categories="illustration design" class="thumb one-fourth">
+                <a href="images/portfolio-images/portfolio-item-2.jpg" data-rel="prettyPhoto" class="screen-roll"><span class="hover_bg"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-2.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Logo Design</h3>
+                </a>
+                <p class="meta">
+                        Design / Illustration
+                </p>
+                </li>
+                <li data-categories="design development" class="thumb one-fourth">
+                <a href="images/portfolio-images/portfolio-item-3.jpg" class="screen-roll" data-rel="prettyPhoto" title="Some Image Dummy Title"><span class="hover_bg"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-3.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Joomla Plugins</h3>
+                </a>
+                <p class="meta">
+                        Design / Development
+                </p>
+                </li>
+                <li data-categories="photography illustration animation" class="thumb one-fourth">
+                <a href="http://www.youtube.com/watch?v=GgR6dyzkKHI" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>SEO Optimization</h3>
+                </a>
+                <p class="meta">
+                        Design / Photography
+                </p>
+                </li>
+                <li data-categories="development design" class="thumb one-fourth">
+                <a href="images/portfolio-images/portfolio-item-5.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-5.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>E-Commerce</h3>
+                </a>
+                <p class="meta">
+                        Design / Development
+                </p>
+                </li>
+                <li data-categories="design animation" class="thumb one-fourth">
+                <a href="http://vimeo.com/7449107" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-6.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Hosting Solutions</h3>
+                </a>
+                <p class="meta">
+                        Design / Animation
+                </p>
+                </li>
+                <li data-categories="development illustration" class="thumb one-fourth">
+                <a href="images/portfolio-images/portfolio-item-7.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-7.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Site Maintenance</h3>
+                </a>
+                <p class="meta">
+                        Design / Illustration
+                </p>
+                </li>
+                <li data-categories="photography animation" class="thumb one-fourth">
+                <a href="http://vimeo.com/7449107" data-rel="prettyPhoto" title="Some Dummy Video Title" class="screen-roll"><span class="hover_bg_video"></span>
+                <div class="bg-grey">
+                        <img src="images/portfolio-images/portfolio-item-8.jpg" alt=" "/>
+                </div>
+                </a>
+                <a href="single-project.html">
+                <h3>Wordpress Plugins</h3>
+                </a>
+                <p class="meta">
+                        Design / Development
+                </p>
+                </li>
+        </ul>
+</div>
+</section>
+<section class="one">
+<div class="title-div">
+        <h2>Our amazing <strong>recent clients</strong></h2>
+        <h5>We've worked with more than 20 startups lorem ipsum dolor sit amet, altera domina tu mihi servitute meam..</h5>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Web Development</span>
+                <img src="images/clients/partner-01.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>graphicriver</span>
+                <img src="images/clients/partner-02.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>codecanyon</span>
+                <img src="images/clients/partner-03.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>photodune</span>
+                <img src="images/clients/partner-04.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth last">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Responsive Design</span>
+                <img src="images/clients/partner-05.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Valid HTML5</span>
+                <img src="images/clients/partner-06.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Jquery Features</span>
+                <img src="images/clients/partner-05.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>CSS3 Animations</span>
+                <img src="images/clients/partner-04.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Development</span>
+                <img src="images/clients/partner-03.jpg" alt="">
+                </a>
+        </div>
+</div>
+<div class="one-fifth last">
+        <div class="thumbnail">
+                <a href="#" class="thumbnail-roll">
+                <span>Graphic Design</span>
+                <img src="images/clients/partner-02.jpg" alt="">
+                </a>
+        </div>
+</div>
+</section>
+</section>
+<!--CONTENT ENDS-->
 
 <?php 
 	 include_once('inc/footer.php');
