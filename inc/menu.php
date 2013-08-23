421547615f7b0ce19fdd06eaf6f5e5e04011979f
zyondias <zyon.dias@hotmail.com>
2013-08-23 13:56:08 -0300

    Desenvolvimento do Menu - Finalizado

--- inc/menu.php
+++ inc/menu.php
@@ -1,91 +1,36 @@
 <section id="top_panel">
 			<div class="container">
 				<ul class="social_icons">
-					<li class="twitter"><a href="#" title="Twitter"></a></li>
-					<li class="facebook"><a href="#" title="Facebook"></a></li>
-					<li class="vimeo"><a href="#" title="Vimeo"></a></li>
-					<li class="googleplus"><a href="#" title="Google+"></a></li>
-					<li class="pinterest"><a href="#" title="Pinterest"></a></li>
-					<li class="linkedin"><a href="#" title="Linkedin"></a></li>
-					<li class="dribbble"><a href="#" title="Dribbble"></a></li>
-					<li class="instagram"><a href="#" title="instagram"></a></li>
-					<li class="skype"><a href="#" title="Skype"></a></li>
-					<li class="rss"><a href="#" title="RSS"></a></li>
+					<li class="twitter"><a href="https://twitter.com/institutors" title="Twitter"></a></li>
+					<li class="facebook"><a href="https://www.facebook.com/pages/Instituto-Raz%C3%A3o-Social/135206043213648" title="Facebook"></a></li>
 				</ul>
 				<ul class="links">
-					<li><a href="#" title="Home">Home</a></li>
-					<li><a href="#" title="About">About us</a></li>
-					<li><a href="#" title="Blog">Blog</a></li>
-					<li><a href="#" title="Portfolio">Portfolio</a></li>
-					<li><a href="#" title="Contact">Contact</a></li>
+					<li><a href="http://www.promon.com.br/exchange" target="_blank"  title="WebMail">WebMail</a></li>
+					<li><a href="http://helpdesk.razaosocial.org.br/admin" target="_blank" title="Help Desk">Help Desk</a></li>
+					<li><a href="http://ead.razaosocial.org.br/login/index.php" target="_blank" title="Ambiente Virtual">Ambiente Virtual</a></li>
 				</ul>
 			</div>
 			</section><section class="container">
 			<header id="header">
 			<div id="sticky_navigation" class="shadow">
 				<div class="container">
-					<a href="index.html" id="logo"></a>
+					<a href="index.php" id="logo"></a>
 					<div id="navigation-wrapper">
 						<nav id="main-navigation">
 						<!--  MAIN  NAVIGATION-->
 						<ul class="main-menu">
-							<li><a href="index.html" title="Home" class="active">Home </a>
-							<ul>
-								<li><a href="index.html">Flex Slider (Default)</a></li>
-								<li><a href="flex-slider-2.html">Flex Slider 2</a></li>
-								<li><a href="homepage-revolution-slider.html">Revolution Slider</a></li>
-								<li><a href="homepage-3d-cute-slider.html">3D Cute Slider</a></li>
-								<li><a href="homepage-alternative.html">Alternative Homepage</a></li>
-								<li><a href="homepage-alternative-2.html">Alternative Homepage 2</a></li>
-								<li><a href="#">Menu Second Level</a>
-								<ul>
-									<li><a href="#">Dummy Item</a></li>
-									<li><a href="#">Dummy Item</a></li>
-								</ul>
-								</li>
-							</ul>
+							<li>
+								<a href="index.php" title="Home" class="active">Home </a>
 							</li>
-							<li><a href="#">Pages</a>
-							<ul>
-								<li><a href="about.html">About Us</a></li>
-								<li><a href="services.html">Our Services</a></li>
-								<li><a href="clients-testimonials.html">Clients &amp; Testimonials</a></li>
-								<li><a href="left-sidebar.html">Left Sidebar</a></li>
-								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="404-error.html">404 Error</a></li>
-							</ul>
+							<li>
+								<a href="#" title="Quem Somos">Quem Somos</a>
 							</li>
-							<li><a href="#">Portfolio</a>
-							<ul>
-								<li><a href="portfolio-two-columns.html">2 Columns Portfolio</a></li>
-								<li><a href="portfolio-three-columns.html">3 Columns Portfolio</a></li>
-								<li><a href="portfolio-three-columns-2.html">3 Columns Portfolio 2</a></li>
-								<li><a href="portfolio-four-columns.html">4 Columns Portfolio (Default)</a></li>
-								<li><a href="portfolio-four-columns-2.html">4 Columns Portfolio 2</a></li>
-								<li><a href="portfolio-five-columns.html">5 Columns Portfolio</a></li>
-								<li><a href="single-project.html">Portfolio Details</a></li>
-								<li><a href="single-project-2.html">Portfolio Details 2</a></li>
-							</ul>
+							<li>
+								<a href="#" title="Projetos">Projetos</a>
 							</li>
-							<li><a href="#">Blog</a>
-							<ul>
-								<li><a href="blog.html">Blog Layout</a></li>
-								<li><a href="blog-post.html">Blog Post Layout</a></li>
-							</ul>
+							<li>
+								<a href="#" title="Contato">Contato</a>
 							</li>
-							<li><a href="#">Features</a>
-							<ul>
-								<li><a href="columns.html">Columns</a></li>
-								<li><a href="typography.html">Typography</a></li>
-								<li><a href="buttons.html">Buttons &amp; Links</a></li>
-								<li><a href="tables-list-icons.html">Tables, Lists &amp; Icons</a></li>
-								<li><a href="pricing.html">Pricing Tables &amp; Boxes</a></li>
-								<li><a href="tabs-toggles-accordions.html">Tabs, Toggles &amp; Accordions</a></li>
-								<li><a href="plugins.html">Awesome Plugins</a></li>
-								<li><a href="dropcaps-message-boxes.html">Dropcaps &amp; Message Boxes</a></li>
-							</ul>
-							</li>
-							<li><a href="contact.html">Contact Us</a></li>
 						</ul>
 						</nav>
 					</div>
