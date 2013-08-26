32b345a1f6f533382263bdb5e8510bda991d1fc9
Fernanda Alfonso <fernanda.alfonsof@gmail.com>
2013-08-26 18:25:19 -0300

    desenvolvimento da pagina do Conviva Educacao

---
+++ conviva-educacao.php
@@ -0,0 +1,134 @@
+<?php
+	include('inc/header.php');
+	include('inc/menu.php');
+?>
+
+<div class="title-div">
+        <div class="container">
+                <div class="projects-details-intro">
+                        <div class="two-third">
+                                <h2>Projetos / <strong>Conviva Educação</strong></h2>
+                        </div>
+                        <div class="one-third last">
+                                <ul class="project-pagination">
+                                        <!--<li><a href="#" class="prev" title="Voltar"><span>Previous</span></a></li>
+                                        <li><a href="#" class="next" title="Avançar"><span>Next</span></a></li>-->
+                                        <li><a href="projetos.php" class="all-projects" title="Todos os Projetos"><span>Projetos</span></a></li>
+                                </ul>
+                        </div>
+                </div>
+        </div>
+</div>
+</section>
+<section id="content" class="container">
+<div class="one-half">
+        <img src="images/projetos/projetos-detalhes/conviva-educacao.png" alt="Conviva Educação"/>
+</div>
+<div class="one-half last">
+        <div class="service-txt">
+                <h3>Conviva Educação</h3>
+                <h4>Em breve</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Accusamus, accusantium, quia, dolorem, voluptates ipsum animi rem unde laborum natus aperiam placeat recusandae 
+                    voluptatum sed beatae ab soluta et libero est!
+                </p>
+                <br/>
+                <p>
+                        <a href="http://convivaeducacao.org.br/" class="button small green circle" target="_blank">Visite o site</a>
+                </p>
+        </div>
+</div>
+    <div class="one">
+            <br />
+            <h4><strong>+ Informações</strong></h4>
+            <ul class="tabs-nav">
+                    <li class="active">
+                    <a href="#tab1">Como funciona?</a>
+                    </li>
+                    <li>
+                    <a href="#tab2">Resultados</a>
+                    </li>
+            </ul>
+            <!-- end .tabs-nav -->
+            <div class="tabs-container">
+                    <div class="tab-content" id="tab1">
+                            <p>
+                                Em breve.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Accusamus, accusantium, quia, dolorem, voluptates ipsum animi rem unde laborum natus aperiam placeat recusandae 
+                    voluptatum sed beatae ab soluta et libero est!
+                            </p>
+                    </div>
+                    <!-- end #tab1 -->
+                    <div class="tab-content" id="tab2">
+                            <p>
+                                Em breve.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Accusamus, accusantium, quia, dolorem, voluptates ipsum animi rem unde laborum natus aperiam placeat recusandae 
+                    voluptatum sed beatae ab soluta et libero est!
+                            </p>
+                    </div>
+                    <!-- end #tab2 -->
+            </div>
+            <!-- end .tabs-container -->
+    </div>
+    <section class="one">
+    <div class="title-div">
+            <h2><strong>Parceiros</strong></h2>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-01.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-02.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-02.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-02.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-02.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>EM BREVE</span>
+                    <img src="images/clients/partner-02.jpg" alt="">
+                    </a>
+            </div>
+    </div>
+    </section>
+    
+    
+</section>
+<?php 
+	 include('inc/footer.php');
+?>
