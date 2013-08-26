6168f5ee146dc17192d764716ddab735baef10d6
zyondias <zyon.dias@hotmail.com>
2013-08-26 18:32:38 -0300

    Desenvolvido pagina curso leitura pelo professor

---
+++ leitura-pelo-professor.php
@@ -0,0 +1,101 @@
+<?php
+	include('inc/header.php');
+	include('inc/menu.php');
+?>
+
+<div class="title-div">
+        <div class="container">
+                <div class="projects-details-intro">
+                        <div class="two-third">
+                                <h2>Projetos / <strong>Curso Leitura pelo Professor</strong></h2>
+                        </div>
+                        <div class="one-third last">
+                                <ul class="project-pagination">
+                                        <!--<li><a href="#" class="prev" title="Voltar"><span>Previous</span></a></li>
+                                        <li><a href="#" class="next" title="Avançar"><span>Next</span></a></li>-->
+                                        <li><a href="projetos.php" class="all-projects" title="Todos os Projetos"><span>Projetos</span></a></li>
+                                </ul>
+                        </div>
+                </div>
+        </div>
+</div>
+</section>
+<section id="content" class="container">
+<div class="one-half">
+        <img src="images/projetos/projetos-detalhes/leitura-pelo-professor.png" alt="Curso leitura pelo Professor"/>
+</div>
+<div class="one-half last">
+        <div class="service-txt">
+                <h3>Curso Leitura pelo Professor</h3>
+                <p>
+                    Curso online de apoio ao trabalho do professor, que objetiva ampliar o repertório de propostas a serem desenvolvidas em sala de aula, a partir de uma concepção de criança como leitora e produtora de cultura.
+                </p>
+        </div>
+</div>
+    <div class="one">
+            <br />
+            <h4><strong>+ Informações</strong></h4>
+            <ul class="tabs-nav">
+                    <li class="active">
+                    <a href="#tab1">Como funciona?</a>
+                    </li>
+                    <li>
+                    <a href="#tab2">Como participar?</a>
+                    </li>
+                    <li>
+                    <a href="#tab3">Resultados</a>
+                    </li>
+            </ul>
+            <!-- end .tabs-nav -->
+            <div class="tabs-container">
+                    <div class="tab-content" id="tab1">
+                            <p>
+                                    São realizadas edições especificas para profissionais dos programas desenvolvidos pelo Instituto em parceria com outras instituições. São oferecidas, ainda, edições para professores e coordenadores pedagógicos do ensino fundamental, que demonstram interesse pelo tema. O conteúdo do curso e a mediação pedagógica são desenvolvidas pelo Instituto Avisa Lá.
+
+                                    O Razão Social, além de responsável pela migração do conteúdo para a mídia online, realiza a coordenação tecnológica das edições.
+
+                                    No segundo semestre de 2011 se realiza uma edição do curso para 120 profissionais de escolas públicas dos municípios do Programa Escola Brasil em diversos estados brasileiros, em parceria com o Instituto Escola Brasil / Santander.
+
+                                    O curso está organizado em torno de atividades em sala de aula em tempo real, estudo de texto, fóruns de discussão e outras atividades virtuais interativas que visam elucidar as principais questões que envolvem a formação do aluno leitor e a aprendizagem da linguagem que se escreve.  
+                            </p>
+                    </div>
+                    <!-- end #tab1 -->
+                    <div class="tab-content" id="tab2">
+
+                    </div>
+                    <!-- end #tab2 -->
+                    <div class="tab-content" id="tab3">
+                          
+                    </div>
+                    <!-- end #tab3 -->
+            </div>
+            <!-- end .tabs-container -->
+    </div>
+    <section class="one">
+    <div class="title-div">
+            <h2><strong>Parceiros</strong></h2>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Avisa Lá</span>
+                    <img src="images/clients/partner-01.jpg" alt="Parceiro Instituto Avisa Lá">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Projeto Escola Brasil</span>
+                    <img src="images/clients/partner-02.jpg" alt="Parceiro Projeto Escola Brasil">
+                    </a>
+            </div>
+    </div>
+    
+    </section>
+    
+    
+</section>
+<?php 
+	 include('inc/footer.php');
+?>
