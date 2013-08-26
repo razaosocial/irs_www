b6afbab8bf7098f6ecbe85261eeb4e85b004f23f
brunasoder <bruna.soder@razaosocial.org.br>
2013-08-26 12:03:05 -0300

    alteracao final pag. projetos

--- projetos.php
+++ projetos.php
@@ -2,28 +2,35 @@
    include_once('inc/header.php');
    include_once('inc/menu.php');
  ?>
-  <div class="title-div">  
+<!-- Mensagem -->
+<!--<div class="title-div">
+        <div class="container">
+                <h2>Mensagem</h2>
+        </div>
+</div>-->
   <div class="container">
+  <div class="title-div"
         <h2>
             RAZAO SOCIAL O CAMINHO PARA O FUTURO!
         </h2><br />
   </div>
+  </div>    
  
-<section id="content" class="container">
-  <div class="filter-portfolio">
+			<section id="content" class="container">
+			<div class="filter-portfolio">
 				<ul id="filterable">
-					<li><a data-categories="*" title="Show All Projects">All</a></li>
-					<li><a data-categories="animation" title="Show Animation Projects">Animation</a></li>
-					<li><a data-categories="design" title="Show All Design Projects">Design</a></li>
-					<li><a data-categories="illustration" title="Show All Illustration Projects">Illustration</a></li>
-					<li><a data-categories="photography" title="Show Photography Projects">Photography</a></li>
-					<li><a data-categories="development" title="Show Development Projects">Development</a></li>
+					<li><a data-categories="*" title="Show All Projects">Em andamento</a></li>
+					<li><a data-categories="animation" title="Show Animation Projects">Finalizadas</a></li>
+					<li><a data-categories="design" title="Show All Design Projects">Categoria 3</a></li>
+					<li><a data-categories="illustration" title="Show All Illustration Projects">Categoria 4</a></li>
+					<li><a data-categories="photography" title="Show Photography Projects">Categoria 5</a></li>
+					<li><a data-categories="development" title="Show Development Projects">Categoria 6</a></li>
 				</ul>
-  </div>
+			</div>
 			<div class="portfolio-grid">
 				<ul class="portfolio-items">
 					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+					<a href="osc.pdf" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
 					</div>
@@ -32,11 +39,11 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
 					<li data-categories="illustration design" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-2.jpg" data-rel="prettyPhoto" class="screen-roll"><span class="hover_bg"></span>
+					<a href="osc.pdf" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-2.jpg" alt=" "/>
 					</div>
@@ -45,11 +52,11 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
 					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-3.jpg" class="screen-roll" data-rel="prettyPhoto" title="Some Image Dummy Title"><span class="hover_bg"></span>
+                                            <a href="osc.pdf" class="screen-roll" ><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-3.jpg" alt=" "/>
 					</div>
@@ -58,24 +65,24 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
-					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+					<li data-categories="photography illustration animation" class="thumb one-fourth">
+					<a href="fer.pdf" class="screen-roll"><span class="hover_bg_video"></span>
 					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+						<img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
 					</div>
 					</a>
 					<a href="single-project.html">
-					<h3>Nome do projeto</h3>
+					<h3>Nome do projeto </h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
 					<li data-categories="development design" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-5.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<a href="osc.pdf" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-5.jpg" alt=" "/>
 					</div>
@@ -84,24 +91,24 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
-					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+					<li data-categories="design animation" class="thumb one-fourth">
+					<a href="osc.pdf" class="screen-roll"><span class="hover_bg_video"></span>
 					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+						<img src="images/portfolio-images/portfolio-item-6.jpg" alt=" "/>
 					</div>
 					</a>
 					<a href="single-project.html">
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
 					<li data-categories="development illustration" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-7.jpg" data-rel="prettyPhoto" title="Some Image Dummy Title" class="screen-roll"><span class="hover_bg"></span>
+					<a href="fer.pdf" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-7.jpg" alt=" "/>
 					</div>
@@ -110,24 +117,24 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
-					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="Some Dummy Title Goes Here" class="screen-roll"><span class="hover_bg"></span>
+					<li data-categories="photography animation" class="thumb one-fourth">
+					<a href="adl.pdf" class="screen-roll"><span class="hover_bg_video"></span>
 					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+						<img src="images/portfolio-images/portfolio-item-8.jpg" alt=" "/>
 					</div>
 					</a>
 					<a href="single-project.html">
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Situação do projeto
 					</p>
 					</li>
 					<li data-categories="illustrator design" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-9.jpg" data-rel="prettyPhoto" title="TÍTULO DA IMAGEM" class="screen-roll"><span class="hover_bg"></span>
+					<a href="adn.pdf" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-9.jpg" alt=" "/>
 					</div>
@@ -136,25 +143,26 @@
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Status do projeto
 					</p>
 					</li>
-					<li data-categories="design development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-1.jpg" data-rel="prettyPhoto" title="TÍTULO DA IMAGEM" class="screen-roll"><span class="hover_bg"></span>
+					<li data-categories="photography animation" class="thumb one-fourth">
+					<a href="fer.pdf" class="screen-roll"><span class="hover_bg_video"></span>
 					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
+						<img src="images/portfolio-images/portfolio-item-10.jpg" alt=" "/>
 					</div>
 					</a>
 					<a href="single-project.html">
 					<h3>Nome do projeto</h3>
 					</a>
 					<p class="meta">
-						Status Projeto
+						Status projeto
 					</p>
 					</li>
 					<li data-categories="design illustration development" class="thumb one-fourth">
-					<a href="images/portfolio-images/portfolio-item-11.jpg" data-rel="prettyPhoto" title="TÍTULO DA IMAGEM" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
+					<a href="osc.pdf" class="screen-roll"><span class="hover_bg"></span>
+                                           
+                                           <div class="bg-grey">
 						<img src="images/portfolio-images/portfolio-item-11.jpg" alt=" "/>
 					</div>
 					</a>
@@ -180,9 +188,10 @@
 					</li>
 				</ul>
 			</div>
+                        </section>                            
 			<!--CONTENT ENDS-->
--</section> 
 			
 <?php 
 	 include_once('inc/footer.php');
 ?>
+  
