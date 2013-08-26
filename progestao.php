e721b4bca4a43992aad434749078bc15d6bc5730
zyondias <zyon.dias@hotmail.com>
2013-08-26 16:22:07 -0300

    Desevolvimento da pagina do Progestão

---
+++ progestao.php
@@ -0,0 +1,126 @@
+<?php
+	include('inc/header.php');
+	include('inc/menu.php');
+?>
+
+<div class="title-div">
+        <div class="container">
+                <div class="projects-details-intro">
+                        <div class="two-third">
+                                <h2>Projetos / <strong>Progestão</strong></h2>
+                        </div>
+                        <div class="one-third last">
+                                <ul class="project-pagination">
+                                        <!--<li><a href="#" class="prev" title="Voltar"><span>Previous</span></a></li>
+                                        <li><a href="#" class="next" title="Avançar"><span>Next</span></a></li>-->
+                                        <li><a href="projetos.php" class="all-projects" title="Todos os Projetos"><span>Projetos</span></a></li>
+                                </ul>
+                        </div>
+                </div>
+        </div>
+</div>
+</section>
+<section id="content" class="container">
+<div class="one-half">
+        <img src="images/projetos/projetos-detalhes/progestao.png" alt="Projeto Progestão Online"/>
+</div>
+<div class="one-half last">
+        <div class="service-txt">
+                <h3>Progestão</h3>
+                <h4>Curso de gestão escolar</h4>
+                <p>
+                        O curso Progestão é uma iniciativa do Consed - Conselho Nacional de Secretários de Educação para formação em gestão administrativa e pedagógica de gestores de escolas públicas.
+                        É um curso a distância que utiliza matérias impressos e tem como objetivo formar lideranças escolares comprometidas com a construção de um projeto de gestão democrática da escola pública, focada no sucesso dos alunos. Visando ampliar as oportunidades de acesso, aprendizado e interação do curso, o Instituto Razão Social uniu-se ao CONSED para desenvolver o Progestão Online. 
+
+                </p>
+                <br/>
+                <p>
+                        <a href="http://consed.org.br/index.php/progestao" class="button small green circle" target="_blank">Visite o site</a>
+                </p>
+        </div>
+</div>
+    <div class="one">
+            <br />
+            <h4><strong>+ Informações</strong></h4>
+            <ul class="tabs-nav">
+                    <li class="active">
+                    <a href="#tab1">Como funciona?</a>
+                    </li>
+                    <li>
+                    <a href="#tab2">Como participar?</a>
+                    </li>
+                    <li>
+                    <a href="#tab3">Resultados</a>
+                    </li>
+            </ul>
+            <!-- end .tabs-nav -->
+            <div class="tabs-container">
+                    <div class="tab-content" id="tab1">
+                            <p>
+                                    O Progestão Online se constrói por meio de um processo democrático e participativo, seguindo os mesmos passos da construção do Progestão, no qual todos aprendem e contribuem. Foi constituído, pelo Consed, um Comitê de Trabalho com representantes das Secretarias de Estado das cinco regiões do país. O objetivo deste grupo é pensar o conteúdo online a partir dos textos originais. Para tanto, elabora-se um mapeamento dos conteúdos dos módulos; a seguir o Razão Social formata as telas a partir de um trabalho de desenho instrucional que contém os conteúdos e interações. Estas propostas são, por sua vez, apresentadas novamente ao Comitê para validação, com apoio dos autores de cada módulo. Utilizando-se de uma plataforma de ensino a distância na internet.
+                            </p>
+                    </div>
+                    <!-- end #tab1 -->
+                    <div class="tab-content" id="tab2">
+
+                    </div>
+                    <!-- end #tab2 -->
+                    <div class="tab-content" id="tab3">
+                          
+                    </div>
+                    <!-- end #tab3 -->
+            </div>
+            <!-- end .tabs-container -->
+    </div>
+    <section class="one">
+    <div class="title-div">
+            <h2><strong>Parceiros</strong></h2>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Gerdau</span>
+                    <img src="images/clients/partner-01.jpg" alt="Parceiro Gerdau">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>IBM</span>
+                    <img src="images/clients/partner-02.jpg" alt="Parceiro IBM">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Instituto Jama</span>
+                    <img src="images/clients/partner-03.jpg" alt="Parceiro Instituto Jama">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Instituto Natura</span>
+                    <img src="images/clients/partner-04.jpg" alt="Parceiro Instituto Natura">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth last">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Santander</span>
+                    <img src="images/clients/partner-05.jpg" alt="Parceiro Santander">
+                    </a>
+            </div>
+    </div>
+    
+    </section>
+    
+    
+</section>
+<?php 
+	 include('inc/footer.php');
+?>
