48a821bd3e979d55b4a144781b3059aac6c1c733
Fernando Nogueira <nando_tecnologia@hotmail.com>
2013-08-26 17:24:46 -0300

    add categorias de projetos

--- projetos.php
+++ projetos.php
@@ -18,215 +18,28 @@
 			<section id="content" class="container">
 			<div class="filter-portfolio">
 				<ul id="filterable">
-					<li><a data-categories="*" title="Show All Projects">Cursos de curta duração</a></li>
-					<li><a data-categories="animation" title="Show Animation Projects">Cursos de média duração</a></li>
-					<li><a data-categories="design" title="Show All Design Projects">Cursos de longa duração</a></li>
-					<li><a data-categories="illustration" title="Show All Illustration Projects">Consultorias Online</a></li>
-					<li><a data-categories="photography" title="Show Photography Projects">Gestão de Portais e Redes</a></li>
-					<li><a data-categories="development" title="Show Development Projects">Ações de Mobilização</a></li>
+					<li><a data-categories="*" title="Show All Projects">Todos</a></li>
+					<li><a data-categories="cursos-de-curta-duracao" title="Cursos de curta duração">Cursos de curta duração</a></li>
+					<li><a data-categories="cursos-de-média-duração" title="Cursos de média duração">Cursos de média duração</a></li>
+                                        <li><a data-categories="cursos-de-longa-duração" title="Cursos de longa duração">Cursos de longa duração</a></li>
+					<li><a data-categories="consultorias-online" title="Consultorias Online">Consultorias Online</a></li>
+					<li><a data-categories="gestao-de-portais-e-redes" title="Gestão de Portais e Redes">Gestão de Portais e Redes</a></li>
+					<li><a data-categories="acoes-de-mobilizacao" title="Ações de Mobilização">Ações de Mobilização</a></li>
 				</ul>
 			</div>
 			<div class="portfolio-grid">
 				<ul class="portfolio-items">
-                                        <!-- CURSOS DE CURTA DURAÇÃO -->
-					<li data-categories="design development" class="thumb one-fourth">
-					<a href="Nome Próprio.php" class="screen-roll"><span class="hover_bg"></span>
+					<li data-categories="consultorias-online" class="thumb one-fourth">
+					<a href="formar-em-rede.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-1.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Nome Próprio</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-					<li data-categories="illustration design" class="thumb one-fourth">
-					<a href="Leitura pelo professor.php" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-2.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Leitura pelo professor</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-					<li data-categories="design development" class="thumb one-fourth">
-                                            <a href="Ambientação Online.php" class="screen-roll" ><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-3.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Ambientação Online</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-                                        <li data-categories="illustration design" class="thumb one-fourth">
-					<a href="Curso d' água.php" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-2.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Curso d' água</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-                                        <!-- CURSOS DE MÉDIA DURAÇÃO -->
-					<li data-categories="photography illustration animation" class="thumb one-fourth">
-					<a href="coordenadores-pedagógicos.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Coordenadores Pedagógicos </h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-					<li data-categories="development design" class="thumb one-fourth">
-					<a href="professores-de-educacao-fisica.pdf" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-5.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Professores de Educação Física</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-                                        <li data-categories="photography illustration animation" class="thumb one-fourth">
-					<a href="diversidade-etnicorracial.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3> Diversidade Étnicorracial </h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-                                        <li data-categories="photography illustration animation" class="thumb one-fourth">
-					<a href="osc.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-4.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3> OSC </h3>
-					</a>
-					<p class="meta">
-						Situação: Em Andamento
-					</p>
-					</li>
-                                        <!-- CURSOS DE LONGA DURAÇÃO -->
-					<li data-categories="design animation" class="thumb one-fourth">
-					<a href="progestao.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-6.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Progestão</h3>
-					</a>
-					<p class="meta">
-						Situação: Em Andamento
-					</p>
-					</li>
-                                        <!-- CONSULTORIAS ONLINE -->
-					<li data-categories="development illustration" class="thumb one-fourth">
-					<a href="alem-das-letras.pdf" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-7.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Além das Letras</h3>
-					</a>
-					<p class="meta">
-						Situação: Em Andamento
-					</p>
-					</li>
-					<li data-categories="photography animation" class="thumb one-fourth">
-					<a href="alem-dos-numeros.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-8.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Além dos Números</h3>
-					</a>
-					<p class="meta">
-						Situação: Em Andamento
-					</p>
-					</li>
-					<li data-categories="illustrator design" class="thumb one-fourth">
-					<a href="formar-em-rede.pdf" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-9.jpg" alt=" "/>
+						<img src="images/projetos/formar-em-rede.png" alt=" "/>
 					</div>
 					</a>
 					<a href="single-project.html">
 					<h3>Formar em Rede</h3>
 					</a>
 					<p class="meta">
-						Status: Em Andamento
-					</p>
-					</li>
-                                         <!-- GESTÃO DE PORTAIS E REDE -->
-					<li data-categories="photography animation" class="thumb one-fourth">
-					<a href="mesa-educadora.pdf" class="screen-roll"><span class="hover_bg_video"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-10.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Programa Mesas Educadoras</h3>
-					</a>
-					<p class="meta">
-						Status: Finalizado
-					</p>
-					</li>
-					<li data-categories="design illustration development" class="thumb one-fourth">
-					<a href="gestao-consed.pdf" class="screen-roll"><span class="hover_bg"></span>
-                                           
-                                           <div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-11.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Porta de Gestão Consed</h3>
-					</a>
-					<p class="meta">
-						Situação: Finalizado
-					</p>
-					</li>
-					<li data-categories="photography development illustration" class="thumb one-fourth">
-					<a href="teste9.pdf" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/portfolio-images/portfolio-item-12.jpg" alt=" "/>
-					</div>
-					</a>
-					<a href="single-project.html">
-					<h3>Nome do projeto</h3>
-					</a>
-					<p class="meta">
-						Status Projeto
+						Situação: Em andamento
 					</p>
 					</li>
 				</ul>
