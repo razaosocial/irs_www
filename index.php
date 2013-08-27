69435172087a37ca1a9b6d15134f0ffd80d444c6
Fernando Nogueira <nando_tecnologia@hotmail.com>
2013-08-27 18:10:46 -0300

    Alterações na ordem da pagina de projetos.

--- index.php
+++ index.php
@@ -21,10 +21,7 @@
                                         <img src="images/slider/slider1.jpg" data-thumb="images/slider/thumbs/thumb1.png" alt=""/>
                                         <div data-type="info" class="cute-caption" data-align="left">
                                                 <div>
-                                                        <h4>Some awesome slide title goes here</h4>
-                                                        <p>
-                                                                Lorem ipsum dolor sit amet, rhenum ibat est se est amet constanter approximavit te. Suos exteriores non dum est se ad suis alteri formam cum magna aliter quem est in.
-                                                        </p>
+                                                        <h4>Tecnologia potencializando iniciativas sociais.</h4>
                                                 </div>
                                         </div>
                                         </li>
@@ -39,7 +36,7 @@
                                                 </div>
                                         </div>
                                         </li>
-                                        <li data-delay="5" data-trans3d="tr4,tr21,tr62" data-trans2d="tr5,tr23,tr30">
+                                        <!--<li data-delay="5" data-trans3d="tr4,tr21,tr62" data-trans2d="tr5,tr23,tr30">
                                         <img src="images/slider/slider3.jpg" data-src="images/slider/slider3.jpg" data-thumb="images/slider/thumbs/thumb3.png" alt=""/>
                                         <div data-type="info" class="cute-caption" data-align="left">
                                                 <div>
@@ -49,7 +46,7 @@
                                                         </p>
                                                 </div>
                                         </div>
-                                        </li>
+                                        </li>-->
                                 </ul>
                                 <ul data-type="controls">
                                         <li data-type="captions"></li>
--- projetos.php
+++ projetos.php
@@ -19,16 +19,13 @@
 			<div class="filter-portfolio">
 				<ul id="filterable">
 					<li><a data-categories="*" title="Show All Projects">Todos</a></li>
-					<li><a data-categories="cursos-de-curta-duracao" title="Cursos de curta duração">Cursos de curta duração</a></li>
-					<li><a data-categories="cursos-de-média-duração" title="Cursos de média duração">Cursos de média duração</a></li>
-                                        <li><a data-categories="cursos-de-longa-duração" title="Cursos de longa duração">Cursos de longa duração</a></li>
-					<li><a data-categories="consultorias-online" title="Consultorias Online">Consultorias Online</a></li>
-					<li><a data-categories="gestao-de-portais-e-redes" title="Gestão de Portais e Redes">Gestão de Portais e Redes</a></li>
+					<li><a data-categories="plataformas-ead" title="Plataformas EAD">Plataformas EAD</a></li>
+					<li><a data-categories="redes-de-aprendizagem" title="Redes de Aprendizagem">Redes de Aprendizagem</a></li>
 				</ul>
 			</div>
 			<div class="portfolio-grid">
 				<ul class="portfolio-items">
-					<li data-categories="consultorias-online" class="thumb one-fourth">
+					<li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
 					<a href="formar-em-rede.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/projetos/formar-em-rede.png" alt="Formar em Rede"/>
@@ -41,59 +38,22 @@
 						Em andamento
 					</p>
 					</li>
-                                        <li data-categories="consultorias-online" class="thumb one-fourth">
-					<a href="alem-dos-numeros.php" class="screen-roll"><span class="hover_bg"></span>
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
+					<a href="ideias-e-praticas-da-gestao-de-organizacoes-sociais.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
-						<img src="images/projetos/adn.png" alt="Além dos Números"/>
+						<img src="images/projetos/osc.png" alt="Ideias e Práticas da Gestão de Organizações Sociais"/>
 					</div>
 					</a>
-					<a href="alem-dos-numeros.php">
-					<h3>Além dos Números</h3>
+					<a href="ideias-e-praticas-da-gestao-de-organizacoes-sociais.php">
+					<h3>Ideias e Práticas da Gestão de Organizações Sociais</h3>
 					</a>
 					<p class="meta">
 						Em andamento
 					</p>
 					</li>
-                                        <li data-categories="cursos-de-longa-duração" class="thumb one-fourth">
-					<a href="progestao.php" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/projetos/progestao.png" alt="Progestão Online"/>
-					</div>
-					</a>
-					<a href="progestao.php">
-					<h3>Progestão Online</h3>
-					</a>
-					<p class="meta">
-						Em andamento
-					</p>
-					</li>
-                                        <li data-categories="gestao-de-portais-e-redes" class="thumb one-fourth">
-					<a href="mesa-educadora.php" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/projetos/mesa-educadora.png" alt="Mesa Educadora"/>
-					</div>
-					</a>
-					<a href="mesa-educadora.php">
-					<h3>Mesa Educadora</h3>
-					</a>
-					<p class="meta">
-						Em andamento
-					</p>
-					</li>
-                                        <li data-categories="cursos-de-curta-duracao" class="thumb one-fourth">
-					<a href="nome-proprio.php" class="screen-roll"><span class="hover_bg"></span>
-					<div class="bg-grey">
-						<img src="images/projetos/nome-proprio.png" alt="Nome Próprio"/>
-					</div>
-					</a>
-					<a href="nome-proprio.php">
-					<h3>Nome Próprio</h3>
-					</a>
-					<p class="meta">
-						Finalizado
-					</p>
-					</li>
-                                        <li data-categories="gestao-de-portais-e-redes" class="thumb one-fourth">
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
 					<a href="conviva-educacao.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/projetos/conviva-educacao.png" alt="Conviva Educação"/>
@@ -106,7 +66,22 @@
 						Em andamento
 					</p>
 					</li>
-                                        <li data-categories="gestao-de-portais-e-redes" class="thumb one-fourth">
+                                        
+                                        <li data-categories="plataformas-ead" class="thumb one-fourth">
+					<a href="progestao.php" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/projetos/progestao.png" alt="Progestão Online"/>
+					</div>
+					</a>
+					<a href="progestao.php">
+					<h3>Progestão Online</h3>
+					</a>
+					<p class="meta">
+						Em andamento
+					</p>
+					</li>
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
 					<a href="trilhas.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/projetos/trilhas.png" alt="TRILHAS"/>
@@ -119,20 +94,22 @@
 						Em andamento
 					</p>
 					</li>
-                                        <li data-categories="cursos-de-curta-duracao" class="thumb one-fourth">
-					<a href="leitura-pelo-professor.php" class="screen-roll"><span class="hover_bg"></span>
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
+					<a href="mesa-educadora.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
-						<img src="images/projetos/leitura-pelo-professor.png" alt="Leitura pelo Professor"/>
+						<img src="images/projetos/mesa-educadora.png" alt="Mesa Educadora"/>
 					</div>
 					</a>
-					<a href="leitura-pelo-professor.php">
-					<h3>Leitura pelo Professor</h3>
+					<a href="mesa-educadora.php">
+					<h3>Mesa Educadora</h3>
 					</a>
 					<p class="meta">
-						Finalizado
+						Em andamento
 					</p>
 					</li>
-                                        <li data-categories="consultorias-online" class="thumb one-fourth">
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
 					<a href="alem-das-letras.php" class="screen-roll"><span class="hover_bg"></span>
 					<div class="bg-grey">
 						<img src="images/projetos/alem-das-letras.png" alt="Além das Letras"/>
@@ -145,6 +122,51 @@
 						Em andamento
 					</p>
 					</li>
+                                        
+                                        <li data-categories="redes-de-aprendizagem" class="thumb one-fourth">
+					<a href="alem-dos-numeros.php" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/projetos/adn.png" alt="Além dos Números"/>
+					</div>
+					</a>
+					<a href="alem-dos-numeros.php">
+					<h3>Além dos Números</h3>
+					</a>
+					<p class="meta">
+						Em andamento
+					</p>
+					</li>
+                                        
+                                        
+                                        <li data-categories="plataformas-ead" class="thumb one-fourth">
+					<a href="nome-proprio.php" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/projetos/nome-proprio.png" alt="Nome Próprio"/>
+					</div>
+					</a>
+					<a href="nome-proprio.php">
+					<h3>Nome Próprio</h3>
+					</a>
+					<p class="meta">
+						Finalizado
+					</p>
+					</li>
+                                        
+                                        <li data-categories="plataformas-ead" class="thumb one-fourth">
+					<a href="leitura-pelo-professor.php" class="screen-roll"><span class="hover_bg"></span>
+					<div class="bg-grey">
+						<img src="images/projetos/leitura-pelo-professor.png" alt="Leitura pelo Professor"/>
+					</div>
+					</a>
+					<a href="leitura-pelo-professor.php">
+					<h3>Leitura pelo Professor</h3>
+					</a>
+					<p class="meta">
+						Finalizado
+					</p>
+					</li>
+                                        
+
 				</ul>
 			</div>
                         </section>                            
