68afa1b726b6f7189d21f8925f824e4f96a0a832
Fernanda Alfonso <fernanda.alfonsof@gmail.com>
2013-08-27 16:45:27 -0300

    mudanca nos parceiros da pagina do projeto conviva e desenvolvimento da pagina de parceiros

---
+++ parceiros.php
@@ -0,0 +1,162 @@
+<?php
+	include('inc/header.php');
+	include('inc/menu.php');
+?>
+
+<section id="content" class="container">
+  <div class="container">
+    <div class="title-div">
+            <h2><strong>Parceiros</strong></h2>
+            <h5>Todos os parceiros nos projetos do Instituto Razão Social</h5>
+    </div>
+</section>
+<section class="one">
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Avisa Lá</span>
+                    <img src="images/projetos/projetos-parceiros/avisala.png" alt="Avisa Lá">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Santander</span>
+                    <img src="images/projetos/projetos-parceiros/santander.png" alt="Santander">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Gerdau</span>
+                    <img src="images/projetos/projetos-parceiros/gerdau.png" alt="Gerdau">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>IBM</span>
+                    <img src="images/projetos/projetos-parceiros/ibm.png" alt="IBM">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth last">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Instituto Jama</span>
+                    <img src="images/projetos/projetos-parceiros/instituto-jama.png" alt="Parceiro Instituto Jama">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Instituto Natura</span>
+                    <img src="images/projetos/projetos-parceiros/instituto-natura.png" alt="Instituto Natura">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Projeto Escola Brasil</span>
+                    <img src="images/projetos/projetos-parceiros/projeto-escola-brasil.png" alt="Projeto Escola Brasil">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação Itaú Social</span>
+                    <img src="images/projetos/projetos-parceiros/fundacao-itau-social.png" alt="Parceiro Fundação Itaú Social">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação Lemann</span>
+                    <img src="images/clients/partner-02.jpg" alt="Parceiro Fundação Lemann">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth last">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação Roberto Marinho</span>
+                    <img src="images/clients/partner-03.jpg" alt="Parceiro Fundação Roberto Marinho">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação SM</span>
+                    <img src="images/clients/partner-04.jpg" alt="Parceiro Fundação SM">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação Telefônica Vivo</span>
+                    <img src="images/clients/partner-05.jpg" alt="Parceiro Fundação Telefônica Vivo">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Fundação Victor Civita</span>
+                    <img src="images/clients/partner-05.jpg" alt="Parceiro Fundação Victor Civita">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Itaú BBA</span>
+                    <img src="images/clients/partner-05.jpg" alt="Parceiro Itaú BBA">
+                    </a>
+            </div>
+    </div>
+    <div class="one-fifth last">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Movimento Todos Pela Educação</p></span>
+                    <img src="images/clients/partner-05.jpg" alt="Parceiro Movimento Todos Pela Educação">
+                    </a>
+            </div>
+    </div>
+    <div class="title-div">
+            <h2><strong>Apoio</strong></h2>
+             <h5>No projeto Conviva Educação</h5>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Consed</p></span>
+                    <img src="images/clients/partner-05.jpg" alt="Apoio Consed">
+                    </a>
+            </div>
+    </div>
+        <div class="title-div">
+            <h2><strong>Gestão</strong></h2>
+             <h5>No projeto Conviva Educação</h5>
+    </div>
+    <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>Undime</p></span>
+                    <img src="images/clients/partner-05.jpg" alt="Apoio Undime">
+                    </a>
+            </div>
+    </div>
+    </section>    
+</section>
+<?php 
+	 include('inc/footer.php');
+?>
