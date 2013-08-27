9c144923bf820c10e8786067217314083a848e8e
brunasoder <bruna.soder@razaosocial.org.br>
2013-08-27 13:47:52 -0300

    teste alt

---
+++ osc.php
@@ -0,0 +1,88 @@
+<?php
+    include ('inc/header.php');
+    include ('inc/menu.php');
+?>
+
+<div class="title-div">
+        <div class="container">
+                <div class="projects-details-intro">
+                        <div class="two-third">
+                                <h2>Projetos / <strong>Ideias e Práticas da Gestão de Organizações Sociais</strong></h2>
+                        </div>
+                        <div class="one-third last">
+                                <ul class="project-pagination">
+                                        <!--<li><a href="#" class="prev" title="Voltar"><span>Previous</span></a></li>
+                                        <li><a href="#" class="next" title="Avançar"><span>Next</span></a></li>-->
+                                        <li><a href="projetos.php" class="all-projects" title="Todos os Projetos"><span>Projetos</span></a></li>
+                                </ul>
+                        </div>
+                </div>
+        </div>
+</div>
+</section>
+<section id="content" class="container">
+<div class="one-half">
+        <img src="images/projetos/projetos-detalhes/osc.png" alt="Ideias e Práticas da Gestão de Organizações Sociais"/>
+</div>
+<div class="one-half last">
+        <div class="service-txt">
+                <h3>Ideias e Práticas da Gestão de Organizações Sociais</h3>
+                <p>
+                       Em Breve
+                </p>
+                <br/>
+                <p>
+                        <a href="http://www.razaosocial.org.br" class="button small green circle" target="_blank">Visite o site</a>
+                </p>
+        </div>
+</div>
+    <div class="one">
+            <br />
+            <h4><strong>+ Informações</strong></h4>
+            <ul class="tabs-nav">
+                    <li class="active">
+                    <a href="#tab1">Como funciona?</a>
+                    </li>
+                    <li>
+                    <a href="#tab3">Resultados</a>
+                    </li>
+            </ul>
+            <!-- end .tabs-nav -->
+            <div class="tabs-container">
+                    <div class="tab-content" id="tab1">
+                            <p>
+                                 Conteúdo do curso.
+                            </p>
+                    </div>
+                    <!-- end #tab2 -->
+                    <div class="tab-content" id="tab3">
+                            <p>
+                                Conteúdo do curso
+                            </p>
+                            <br />
+
+                    </div>
+                    <!-- end #tab3 -->
+            </div>
+            <!-- end .tabs-container -->
+    </div>
+    <section class="one">
+    <div class="title-div">
+            <h2><strong>Parceiros</strong></h2>
+    </div>
+   <div class="one-fifth">
+            <div class="thumbnail">
+                    <a href="#" class="thumbnail-roll">
+                    <span>IBM</span>
+                    <img src="images/clients/partner-02.jpg" alt="Parceiro IBM">
+                    </a>
+            </div>
+    </div>
+    </div>
+    </section>
+    
+    
+</section>
+<?php 
+	 include('inc/footer.php');
+?>
